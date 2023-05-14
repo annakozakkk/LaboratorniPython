4973dae3a7fcc8789b4af1a495034c65b828c7a9
annakozakkk <kozakanna011@gmail.com>
2023-05-14 16:20:02 +0300

    added some changes

--- Bicycle.py
+++ Bicycle.py
@@ -1,45 +1,42 @@
 class Bicycle:
-    type = ""
-    brand = ""
-    maxSpeed = 0
-    currentSpeed = 0
+    _type = ""
+    _brand = ""
+    _max_speed = 0
+    _current_speed = 0
 
-    def __init__(self, type="None", brand="None", maxSpeed=0, currentSpeed=0):
-        self.type = type
-        self.brand = brand
-        self.maxSpeed = maxSpeed
-        self.currentSpeed = currentSpeed
+    def __init__(self, type="None", brand="None", max_speed=0, current_speed=0):
+        self._type = type
+        self._brand = brand
+        self._max_speed = max_speed
+        self._current_speed = current_speed
 
-    @classmethod
-    def get_instance(cls):
-        return cls("mountain", "Trek", 50, 30)
+    @staticmethod
+    def get_instance():
+        Bicycle.instance = Bicycle("road", "Colnago", 40, 12)
+        return Bicycle.instance
 
     def accelerate(self, speed):
-        if speed < self.maxSpeed:
-            self.currentSpeed += speed
-            return self.currentSpeed
+        if speed < self._max_speed:
+            self._current_speed += speed
+            return self._current_speed
         else:
-            return self.maxSpeed
+            return self._max_speed
 
     def brake(self):
-        self.currentSpeed = 0
-        return self.currentSpeed
+        self._current_speed = 0
+        return self._current_speed
 
-    def slowDown(self, speed):
-        if speed <= self.currentSpeed:
-            self.currentSpeed -= speed
-            return self.currentSpeed
+    def slow_down(self, speed):
+        if speed <= self._current_speed:
+            self._current_speed -= speed
+            return self._current_speed
         return self.brake()
 
     def __str__(self):
-        return f"Bicycle: type={self.type}, brand={self.brand}, maxSpeed={self.maxSpeed}, currentSpeed={self.currentSpeed}"
+        return f"Bicycle: type={self._type}, brand={self._brand}, maxSpeed={self._max_speed}, currentSpeed={self._current_speed}"
 
 
-bicycles = []
-bicycles.append(Bicycle("road", "Trek", 40, 23))
-bicycles.append(Bicycle())
-bicycles.append(Bicycle.get_instance())
-bicycles.append(Bicycle.get_instance())
+bicycles = [Bicycle("road", "Trek", 40, 23), Bicycle(), Bicycle.get_instance(), Bicycle.get_instance()]
 
 for bicycle in bicycles:
     print(bicycle)

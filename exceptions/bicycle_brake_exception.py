45847bc301e72faa3fdb4a028e680f65a1b09b63
annakozakkk <kozakanna011@gmail.com>
2023-06-04 00:30:30 +0300

    added 10 lab-working with exceptions

---
+++ exceptions/bicycle_brake_exception.py
@@ -0,0 +1,7 @@
+class BicycleBrakeException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f'BicycleBrakeException: {self.message}'
